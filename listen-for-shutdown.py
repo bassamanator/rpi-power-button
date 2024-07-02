a27e2f2f7b426d5ad6d8f7ff16d1fb0b3f09225f
Bassam Husain <bassam.husain@gmail.com>
2024-07-02 18:14:36 -0400

    More adjustments to use gpiozero.

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import subprocess
 import time
-from gpiozero import Button
+from gpiozero import Button, LED
+
+# from gpiozero import LED
+
 
 print("Raspberry PI GPIO Button & LED Shutdown Script")
 
@@ -11,66 +14,32 @@ PIN_LED = 4  # GPIO4 / Pin #7
 PIN_BUTTON = 3  # GPIO3 / Pin #5
 TIME_PRESSED_MAX = 3  # seconds
 
+LED_ = LED(PIN_LED)
+POWER_BUTTON = Button(PIN_BUTTON, hold_time=TIME_PRESSED_MAX)
+
 try:
 
     def button_pressed():
         print("button was pressed")
-        led_state = GPIO.LOW
-
         # Blink LED
-        GPIO.output(PIN_LED, led_state)  # blink LED
-        led_state = not led_state
-        time.sleep(0.1)  # loop time and led blink interation rate.
+        while True:
+            LED_.off()
+            time.sleep(0.1)
+            LED_.on()
+            time.sleep(0.1)
 
     def button_released():
         print("button was released")
-        led_state = GPIO.HIGH
-        GPIO.output(PIN_LED, led_state)  # LED turn on
+        LED_.on()
 
     def do_shutdown():
         print("Shutting down...")
         subprocess.call(["shutdown", "-h", "now"], shell=False)  # Power Off
 
-    # Button Press Logic
-    def button_interupt(channel):
-        time_start = time.time()
-        time_button = 0
-        led_state = GPIO.LOW
-
-        # Loop while button is pressed, and not passed max time
-        # wait for the button up
-        while GPIO.input(channel) == GPIO.LOW and time_button <= TIME_PRESSED_MAX:
-            # DEBUGGING OUTPUT
-            print("Button:", time_button, led_state)
-
-            # Blink LED
-            GPIO.output(PIN_LED, led_state)  # blink LED
-            led_state = not led_state
-            time.sleep(0.1)  # loop time and led blink interation rate.
-
-            # How long was the button down?
-            time_button = time.time() - time_start
-
-        # Set LED back to High, just in case was low.
-        GPIO.output(PIN_LED, GPIO.HIGH)  # blink LED
-
-        # Determine Button Time
-        if time_button >= TIME_PRESSED_MAX:
-            print("Power Button Pressed & Held:", time_button)
-            subprocess.call(["shutdown", "-h", "now"], shell=False)  # Power Off
-
-    # Ignore Warrnings or not
-    GPIO.setwarnings(False)  # Ignore warning for now
-
-    # Setting GPIO layout
-    # GPIO.setmode(gpio.BOARD) | Use boards header pin order or lableing GPIO##.
-    # https://iot4beginners.com/difference-between-bcm-and-board-pin-numbering-in-raspberry-pi/
-    GPIO.setmode(GPIO.BCM)
-
-    # # Set pin as input pin pulled down to GND
-    GPIO.setup(PIN_LED, GPIO.OUT, initial=GPIO.HIGH)  # LED ON
+    LED_.on()
+    # GPIO.setup(PIN_LED, GPIO.OUT, initial=GPIO.HIGH)  # LED ON
     # GPIO.setup(PIN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button
-    POWER_BUTTON = Button(PIN_BUTTON, hold_time=TIME_PRESSED_MAX)
+    # POWER_BUTTON = Button(PIN_BUTTON, hold_time=TIME_PRESSED_MAX)
 
     # # Button Press Event
     # GPIO.add_event_detect(
@@ -91,4 +60,3 @@ except KeyboardInterrupt:
     print("CTRL+C")
 finally:
     print("End program")
-    GPIO.cleanup()
